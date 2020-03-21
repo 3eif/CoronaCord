3e0103af36960c3b276f37513e130c834a56a666
Tetra <seifaziz10@gmail.com>
2020-03-21 13:10:08 -0700

    Fixed countries command

--- commands/statistics/countries.js
+++ commands/statistics/countries.js
@@ -1,18 +1,10 @@
 /* eslint-disable no-unused-vars */
 const Discord = require("discord.js");
-const novelcovid = require("coronacord-api-wrapper");
-const { post } = require("snekfetch");
 
 module.exports = {
   name: "countries",
   description: "Shows a list of supported countries and country names.",
   async execute (client, message, args) {
-    const countryStats = await novelcovid.countries();
-    let countries = "";
-    countryStats.forEach(country => { countries += `${country.country}\n`; });
-    console.log(countries);
-    const { body } = await post("https://www.hastebin.com/documents").send(countries);
-
     const embed = new Discord.MessageEmbed()
       .setAuthor("Supported Countries", client.settings.avatar)
       .setTitle("All supported countries and names can be found here")
